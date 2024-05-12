6288c332910ae44f4e8b3761742c1ad58329d039
wang huai <heibaimeng@gmail.com>
2024-05-12 12:44:48 +0000

    引入 multer ，新增文件上传接口

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ require('dotenv').config()
 
 var app = express();
 
+const multer = require('multer')
+const upload = multer()
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 
